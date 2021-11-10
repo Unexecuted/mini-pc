dffb32c67479b19560b652a80277cd880949279c
Ken S <73849432+Unexecuted@users.noreply.github.com>
2021-11-10 14:11:51 +0000

    -Cleaned 'interface' -Shortened 'main' +Planning on simplifying 'commands'

--- commands.py
+++ commands.py
@@ -1,3 +1,6 @@
+import smtplib
+
+#'terminal' commands
 def welcome():
   print("Welcome to mini pc! Use the PC_ON and PC_OFF commands to turn the pc on and off, and the help command to generate a list of current commands!\n")
 
@@ -6,8 +9,8 @@ def my_help():
   if ask == '1' or ask == '1.':
     print('PC_OFF, help, echo, forgot_pass, battery')
 
-def echo(user):
-  print(user)
+def echo(message):
+  print(message)
 
 def forgot_pass(user):
   return 1
@@ -21,3 +24,16 @@ def battery(battery):
 
 def change_color(color, background):
   print('test')
+
+
+#apps
+def email(sendto, message):
+  email = 'replitfeedback@gmail.com' # Your email
+  password = 'replitfeedback12' # Your email account password
+  send_to_email = str(sendto) # Who you are sending the message to
+
+  server = smtplib.SMTP('smtp.gmail.com', 587) # Connect to the server
+  server.starttls() # Use TLS
+  server.login(email, password) # Login to the email server
+  server.sendmail(email, send_to_email , message) # Send the email
+  server.quit() # Logout of the email server
--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from commands import welcome, echo, commands, my_help, battery
 def main():
   welcome()
 
-
   pc = False
 
   while pc == False:
@@ -19,7 +18,6 @@ def main():
       print("\nPC Successfully Started!")
     elif response == "PC_OFF":
       print("\nPC is not yet on...")
-
 
   while pc == True:
     bat_level = 100
@@ -41,7 +39,6 @@ def main():
     logged_in = True
     print(login_info)
 
-
     while logged_in == True:
       cmd = input(">")
       if 'echo' in cmd[:5]:
@@ -60,58 +57,9 @@ def main():
         battery(bat_level)
       else:
         print('Unrecognized command; Please try again or type help for a list of commands.')
-
-
-  pc = False
-
-  while pc == False:
-    response = input("")
-    if response == "PC_ON":
-      pc = True
-      print("\nPC Successfully Started!")
-    elif response == "PC_OFF":
-      print("\nPC is not yet on...")
-
-
-  while pc == True:
-    bat_level = 100
-    user_list = []
-    pass_list = []
-
-    if user_list == []:
-      logged_in = False
-      print('\nSince this is your first time logging on, please create a new Username and Password.\n')
-
-    login_info = {'Usernames' : [], 'Passwords' : []}
-    username = input("Username: ")
-    password = getpass.getpass("Password: ")
-    user_list.append(username)
-    pass_list.append(password)
-
-    login_info['Usernames'] = user_list
-    login_info['Passwords'] = pass_list
-    logged_in = True
-    print(login_info)
-
-
-    while logged_in == True:
-      cmd = input(">")
-      if 'echo' in cmd[:5]:
-        if 'echo ' in cmd[:5]:
-          commands(cmd)
-        else:
-          print('Missing argument: echo (input)')
-      elif 'PC_OFF' in cmd:
-        logged_in = False
+      bat_level -= 1
+      if bat_level == 0:
         pc = False
-      elif 'help' in cmd[:4]:
-        my_help()
-      elif 'clear' in cmd[:5]:
-        replit.clear()
-      elif 'battery' in cmd[:7]:
-        battery(bat_level)
-      else:
-        print('Unrecognized command; Please try again or type help for a list of commands.')
-  
+
 if __name__ == "__main__":
   main()
